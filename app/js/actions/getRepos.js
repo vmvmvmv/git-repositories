d7fd1d1ed3658bb04d539ad7e081b2056c6f3b46
vst-wld <vst@alterpost.org>
2017-08-02 19:04:52 +0300

    fix req url

--- app/js/actions/getRepos.js
+++ app/js/actions/getRepos.js
@@ -8,11 +8,12 @@ export function getReposSuccess(reposes) {
 }
 export function fetchReposData(language) {
     return (dispatch) => {
-        var ecnodeURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language: 
-        ${language}&sort=stars&order=desc&type=Repositories`);
+        var ecnodeURI = window.encodeURI("https://api.github.com/search/repositories?q=stars:>1+language:" 
+       +language+"&sort=stars&order=desc&type=Repositories");
 
         return axios.get(ecnodeURI)
             .then((response) => {
+                console.log(response)
                 dispatch(getReposSuccess(response.data.items))
             })
     }
