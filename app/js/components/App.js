77d12407700ff320d38f657962e74ce12a449b51
vladislav.tabala <vladislav.tabala@x2sy.com>
2017-12-11 14:25:39 +0200

    gitignore

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => {
     }
 };
 
-const mapDispathToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
     return {
         getRepos: (language) => dispatch(getReposRequest(language))
     }
